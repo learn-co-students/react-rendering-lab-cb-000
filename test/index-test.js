1048cd7e5d818c882e21afff1688bcfd13990471
joel <joelbitar@protonmail.com>
2017-11-05 23:43:28 -0500

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import sinon from 'sinon';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 import Rating from '../src/components/Rating';
 import Circle from '../src/components/Circle';
