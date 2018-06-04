14f357848febc7809b121bda55056347bff3fb0f
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2018-06-04 00:14:46 +0000

    Done.

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -30,6 +30,10 @@ class Animation extends React.Component {
     progressBar.className = "off on";
     setTimeout(() => (progressBar.className = "off"), 1100);
   };
+  
+  componentWillUpdate() {
+    this.showLoadingBar()
+  }
 
   render() {
     return (
